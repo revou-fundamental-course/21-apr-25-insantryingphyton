110f9ed2becf8d00c3c5aa637576c79dfcdd51b9
insantryingphyton <al.insan.wijaya@gmail.com>
2025-05-01 17:42:10 +0700

    revou project change 17

--- js/script.js
+++ js/script.js
@@ -1,12 +1,16 @@
-console.log('JavaScript working?')
+console.log('JavaScript working?');
 
-console.log(document.getElementById('header-id').innerText)
+console.log(document.getElementById('header-id').innerText);
 
 function validateForm() {
     const inputCelcius = document.getElementById('konversi-input');
     console.log(inputCelcius.value);
 
-    if(inputCelcius.value === '') {
-        alert('Input tidak boleh kosong')
+    if (inputCelcius.value === '') {
+        alert('Input tidak boleh kosong');
+    }
+
+    else {
+        /// Convert to fahrenheit
     }
 }
